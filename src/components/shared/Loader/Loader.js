fbc24a21e3293956984cb81abe217c1e1848eea4
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-03-20 20:21:00 +0100

    fix for loader

--- src/components/shared/Loader/Loader.js
+++ src/components/shared/Loader/Loader.js
@@ -4,30 +4,9 @@ const SpinnerPage = () => {
   return (
     <>
       <div className={styles.loaderContainer}>
-        <div
-          className={`${styles.spinnerGrow} ${styles.textDanger}`}
-          role='status'
-        >
-          <span className={styles.srOnly}>Loading...</span>
-        </div>
-        {/* <div
-          className={`${styles.spinnerGrow} ${styles.textDanger}`}
-          role='status'
-        >
-          <span className={styles.srOnly}>Loading...</span>
-        </div>
-        <div
-          className={`${styles.spinnerGrow} ${styles.textDanger}`}
-          role='status'
-        >
-          <span className={styles.srOnly}>Loading...</span>
-        </div> */}
-        {/* <div className='spinner-grow text-danger' role='status'>
-          <span className='sr-only'>Loading...</span>
-        </div>
         <div className='spinner-grow text-danger' role='status'>
           <span className='sr-only'>Loading...</span>
-        </div> */}
+        </div>
       </div>
     </>
   );
