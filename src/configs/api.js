cbbf9a6a8e7cf3bff07a0aa336beb51216cc8d64
Jakub Pisula <jakubpisula@macbook-pro-jakub.home>
2022-04-26 22:40:05 +0200

    Switched API URL

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,4 @@
-// export const API_URL = 'http://localhost:49153/api/v1';
-// export const API_UPLOADS_URL = 'http://localhost:49153/uploads/photos';
-export const API_URL = 'https://oyama-karate-api.herokuapp.com/api/v1';
+export const API_URL =
+  'https://oyama-karate-server.herokuapp.com/api/v1/homepage';
 export const API_UPLOADS_URL =
-  'https://oyama-karate-api.herokuapp.com/uploads/photos';
+  'https://oyama-karate-server.herokuapp.com/uploads/photos';
