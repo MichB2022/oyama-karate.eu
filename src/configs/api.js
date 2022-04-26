1a3ac3d0fff735434e30a6a4d1f94479bab90e62
Jakub Pisula <jakubpisula@macbook-pro-jakub.home>
2022-04-26 22:42:29 +0200

    a

--- src/configs/api.js
+++ src/configs/api.js
@@ -2,3 +2,4 @@ export const API_URL =
   'https://oyama-karate-server.herokuapp.com/api/v1/homepage';
 export const API_UPLOADS_URL =
   'https://oyama-karate-server.herokuapp.com/uploads/photos';
+//
