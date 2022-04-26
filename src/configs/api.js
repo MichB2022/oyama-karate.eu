8da9e7f38b7f395203fee1e4f04e1d84744b9f18
Jakub Pisula <jakub.pisula@wp.pl>
2022-04-26 22:58:45 +0200

    fix

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,4 @@
-export const API_URL =
-  'https://oyama-karate-server.herokuapp.com/api/v1/homepage';
+export const API_URL = 'https://oyama-karate-server.herokuapp.com/api/v1';
 export const API_UPLOADS_URL =
   'https://oyama-karate-server.herokuapp.com/uploads/photos';
 //
